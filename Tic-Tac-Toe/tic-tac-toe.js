9d747002612f85e173e7210d1a15e147cf6fbd0b
Brady Barker <brady.d.barker@gmail.com>
2023-12-27 23:26:05 -0500

    Created all factory functions (besides DOM manipulation) functions: player, gameBoard, tttGame. Game loop started, can accept marker placements. No error handling yet. Need to move to DOM later

--- Tic-Tac-Toe/tic-tac-toe.js
+++ Tic-Tac-Toe/tic-tac-toe.js
@@ -8,5 +8,54 @@ function player(m, my_turn=false) {
 }
 
 gameBoard = (function() {
+    const board = [[' ',' ',' '], [' ',' ',' '], [' ',' ',' ']];
 
+    reset_board = function() {
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                board[i][j] = ' ';
+            }
+        }
+    }
+
+    place_marker = function(player) {
+        // Getting input from the player, where to place marker needs to be index
+        index1 = prompt("Index 1: needs to be 0-2");
+        index2 = prompt("Index 2: Needs to be 0-2");
+        
+        board[index1][index2] = player.marker()
+        gameBoard.display_board();
+    }
+
+    display_board = function() {
+        for (let i = 0; i < board.length; i++) {
+            console.log(...board[i]);
+        }
+    }
+
+    return { reset_board, place_marker, display_board }
 })();
+
+tttGame = (function(player1, player2) {
+    start_game = function() {
+        while (!check_win()) {
+            let current_player = (player1.my_turn) ? player1 : player2;
+
+            gameBoard.display_board();
+            gameBoard.place_marker(current_player);
+            break;
+        }
+    }
+
+    check_win = function() {
+
+    }
+
+    reset_game = function() {
+
+    }
+
+    return { start_game }
+})(player('X', true), player('O'));
+
+tttGame.start_game()
