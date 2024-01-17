90bb604f113caef0360305e0a2ffc6305a987fe9
Brady Barker <brady.d.barker@gmail.com>
2024-01-16 21:21:50 -0500

    Installed css loader for webpack

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,10 +1,15 @@
 import _ from 'lodash'
+// import function from another file
+import myName from './myName';
 
 function component() {
     const element = document.createElement('div');
 
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    // Use imported function
+    element.textContent = myName('Cody');
 
     return element;
 }
