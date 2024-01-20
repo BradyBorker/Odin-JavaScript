030e74ca3752eb741f51489b49efd80306930450
Brady Barker <brady.d.barker@gmail.com>
2024-01-20 13:18:09 -0500

    Learning how to use production webpack

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,25 +1,17 @@
-import _ from 'lodash'
-import './style.css'
-import Icon from './icon.png'
-
-// import function from another file
-import myName from './myName';
+import _ from 'lodash';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello')
 
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
-    element.appendChild(myIcon);
-
-    // Use imported function
-    // element.textContent = myName('Cody');
+    element.appendChild(btn);
 
     return element;
 }
