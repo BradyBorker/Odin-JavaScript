37704cae08a66d86b8dc1c545af3755cfed2a2a2
Brady Barker <brady.d.barker@gmail.com>
2024-03-16 16:10:46 -0400

    done

--- js-dynamic-user-interface-interactions/drop-down-menu/src/index.js
+++ js-dynamic-user-interface-interactions/drop-down-menu/src/index.js
@@ -1,1 +1,20 @@
 import './style.css';
+import { addClass, removeClass } from './dom';
+
+const dropDownTitles = document.querySelectorAll('.drop-down-title');
+dropDownTitles.forEach((dropDownTitle) => {
+  dropDownTitle.addEventListener('mouseover', (e) => {
+    const dropDownItems = e.target.parentNode.querySelector('.drop-down-items');
+    addClass(dropDownItems, 'visible');
+  });
+});
+
+const dropDownContainers = document.querySelectorAll('.drop-down-container');
+dropDownContainers.forEach((container) => {
+  container.addEventListener('mouseleave', (e) => {
+    const dropDownItems = e.target.querySelector('.drop-down-items');
+    if (dropDownItems.classList.contains('visible')) {
+      removeClass(dropDownItems, 'visible');
+    }
+  });
+});
