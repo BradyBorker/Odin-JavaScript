3cb4e27aab0d05a6b7153a917e4f1e6af6a95d1a
Brady Barker <brady.d.barker@gmail.com>
2024-03-22 21:29:34 -0400

    done

--- js-dynamic-user-interface-interactions/image-slider/src/dom.js
+++ js-dynamic-user-interface-interactions/image-slider/src/dom.js
@@ -1,3 +1,91 @@
 export function setFrameWidth(element, frameWidth) {
-    element.style.width = `${frameWidth}px`
+  element.style.width = `${frameWidth}px`;
+}
+
+function highLightNavDot(goalPosition) {
+  const navDots = document.querySelectorAll(".nav-dot");
+  navDots.forEach((navDot) => {
+    navDot.classList.remove("highlighted");
+  });
+
+  const navDot = navDots[Number(goalPosition)];
+  navDot.classList.add("highlighted");
+}
+
+export function addNavDots(navDotsContainer, slidesCount, moveSlides) {
+  for (let count = 0; count < slidesCount; count += 1) {
+    const navDot = document.createElement("div");
+    navDot.classList.add("nav-dot");
+    navDot.id = `${count}`;
+
+    navDot.addEventListener("click", (e) => {
+      const goalPosition = e.target.id;
+      moveSlides.toPosition(goalPosition);
+
+      highLightNavDot(Number(e.target.id));
+
+      e.target.classList.add("highlighted");
+    });
+
+    navDotsContainer.appendChild(navDot);
+  }
+
+  highLightNavDot(0);
+}
+
+export function slideTransitions(slidesContainer, slideElements, frameWidth) {
+  let currentPosition = 0;
+  const positions = [0];
+
+  for (let count = 1; count < slideElements.length; count += 1) {
+    positions.push(Number(positions.slice(-1)) - Number(frameWidth));
+  }
+
+  const toBeginning = () => {
+    slidesContainer.style.transform = `translateX(0px)`;
+  };
+
+  const toEnd = () => {
+    const distance = Number(positions.slice(-1));
+    slidesContainer.style.transform = `translateX(${distance}px)`;
+  };
+
+  const left = (times) => {
+    const currentX = positions[currentPosition];
+    const distance = currentX + times * frameWidth;
+
+    if (currentPosition - 1 < 0) {
+      toEnd();
+      currentPosition = positions.length - 1;
+    } else {
+      slidesContainer.style.transform = `translateX(${distance}px)`;
+      currentPosition -= 1;
+    }
+
+    highLightNavDot(Number(currentPosition));
+  };
+
+  const right = (times) => {
+    const currentX = positions[currentPosition];
+    const distance = currentX - times * frameWidth;
+
+    if (currentPosition + 1 > positions.length - 1) {
+      toBeginning();
+      currentPosition = 0;
+    } else {
+      slidesContainer.style.transform = `translateX(${distance}px)`;
+      currentPosition += 1;
+    }
+
+    highLightNavDot(Number(currentPosition));
+  };
+
+  const toPosition = (goalPosition) => {
+    const distance = positions[goalPosition];
+    currentPosition = goalPosition;
+
+    slidesContainer.style.transform = `translateX(${distance}px)`;
+  };
+
+  return { left, right, toPosition };
 }
--- js-dynamic-user-interface-interactions/image-slider/src/index.js
+++ js-dynamic-user-interface-interactions/image-slider/src/index.js
@@ -1,16 +1,27 @@
-import { setFrameWidth } from "./dom";
+import { setFrameWidth, slideTransitions, addNavDots } from "./dom";
 import "./style.css";
 
-const frameElement = document.querySelector('.frame');
-const frameWidth = '500';
+const frameElement = document.querySelector(".frame");
+const frameWidth = 500;
 setFrameWidth(frameElement, frameWidth);
 
-const slideElements = document.querySelectorAll('.slides > .slide');
+const slideElements = document.querySelectorAll(".slides > .slide");
 slideElements.forEach((element) => {
-    setFrameWidth(element, frameWidth);
-})
+  setFrameWidth(element, frameWidth);
+});
 
-const leftArrow = document.querySelector('.left-arrow');
-leftArrow.addEventListener('click', () => {
+const slidesContainer = document.querySelector(".slides");
+const moveSlides = slideTransitions(slidesContainer, slideElements, frameWidth);
 
-})
+const leftArrow = document.querySelector(".left-arrow");
+leftArrow.addEventListener("click", () => {
+  moveSlides.left(1);
+});
+
+const rightArrow = document.querySelector(".right-arrow");
+rightArrow.addEventListener("click", () => {
+  moveSlides.right(1);
+});
+
+const navDotsContainer = document.querySelector(".nav-dots-container");
+addNavDots(navDotsContainer, slidesContainer.children.length, moveSlides);
