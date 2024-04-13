657721c3020e856c0daf0b84e4bbe4127bb1e7f6
Brady Barker <brady.d.barker@gmail.com>
2024-04-13 18:02:14 -0400

    Init new branch

--- Library/library.js
+++ Library/library.js
@@ -22,12 +22,12 @@ class Library {
         this.display();
     }
 
-    static addEditReadStatusListener(property, book, td) {    
+    static addEditReadStatusListener(property, book, td) {
         td.addEventListener('click', (e) => {
             if (book.read === 'No') {
                 book[property] = 'Yes';
                 td.textContent = 'Yes';
-    
+
             } else if (book.read === 'Yes') {
                 book[property] = 'No';
                 td.textContent = 'No';
@@ -41,7 +41,7 @@ class Library {
         td.appendChild(document.createTextNode("X"));
         td.addEventListener('click', (e) => {
             myLibrary.removeBook();
-            
+
         })
         tr.appendChild(td);
     }
@@ -51,30 +51,30 @@ class Library {
         while (tableBody.firstChild) {
             tableBody.removeChild(tableBody.firstChild)
         }
-        
+
         let bookNumber = 0;
         for (let book of this.books) {
             const tr = document.createElement("tr");
             tableBody.appendChild(tr);
-            
+
             for (let property of Object.keys(book)) {
                 let td = document.createElement("td");
                 let tdText = document.createTextNode(book[property]);
-    
+
                 td.appendChild(tdText);
                 tr.appendChild(td);
-    
+
                 if (property === 'read') {
                     Library.addEditReadStatusListener(property, book, td);
                 }
             }
-    
+
             Library.addBookRemovalListener(tr, bookNumber)
             bookNumber++;
         }
     }
 }
-myLibrary = new Library
+const myLibrary = new Library
 myLibrary.display();
 
 function allInputsFilled(author, title, pageCount) {
@@ -109,7 +109,7 @@ form.addEventListener('submit', (e) => {
     let title = document.querySelector("#title");
     let pageCount = document.querySelector("#page-count");
     let read = document.querySelector("input[name=has-read]:checked");
-    
+
     if (e.submitter.id === 'add' && allInputsFilled(author.value, title.value, pageCount.value)) {
         myLibrary.addBook = new Book(author.value, title.value, pageCount.value, read.id);
         dropModal(author, title, pageCount, modal, library);
