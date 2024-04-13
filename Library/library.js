60c794c20cecb16238bfdb90c4261ca24621c08e
Brady Barker <brady.d.barker@gmail.com>
2024-04-13 19:16:47 -0400

    Sweet and simple

--- Library/library.js
+++ Library/library.js
@@ -75,27 +75,8 @@ class Library {
     }
 }
 
-(function formValidations() {
-    const authorInput = document.querySelector('#author');
-    authorInput.addEventListener('input', () => {
-        if (!authorInput.validity.valid) {
-            authorInput.setCustomValidity('Please enter an authors name');
-        }
-    })
-})()
-
 const myLibrary = new Library
 myLibrary.display();
-
-/*
-function allInputsFilled(author, title, pageCount) {
-    if (author === '' || title === '' || pageCount === '') {
-        alert("All inputs need to be filled")
-        return false;
-    }
-    return true;
-}
-*/
 
 function dropModal(author, title, pageCount, modal, library) {
     author.value = '';
@@ -122,12 +103,15 @@ form.addEventListener('submit', (e) => {
     let pageCount = document.querySelector("#page-count");
     let read = document.querySelector("input[name=has-read]:checked");
 
-    //if (e.submitter.id === 'add' && allInputsFilled(author.value, title.value, pageCount.value)) {
     if (e.submitter.id === 'add') {
-        myLibrary.addBook = new Book(author.value, title.value, pageCount.value, read.id);
-        dropModal(author, title, pageCount, modal, library);
+        if (e.target.checkValidity()) {
+            myLibrary.addBook = new Book(author.value, title.value, pageCount.value, read.id);
+            dropModal(author, title, pageCount, modal, library);
+        }
     } else if (e.submitter.id === 'discard') {
         dropModal(author, title, pageCount, modal, library);
     }
 });
+
+
 
