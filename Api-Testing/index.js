9fbfcea47eb14a67fb81d6f74e2dbfea178effc9
Brady Barker <brady.d.barker@gmail.com>
2024-04-23 21:19:13 -0400

    Init and done

---
+++ Api-Testing/index.js
@@ -0,0 +1,34 @@
+const img = document.querySelector('img');
+function getGif(search) {
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=zQZfa80h0Q8Z2Q6I4jGTA54kHQLTBC53&s=${search}`, { mode: 'cors' })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            if (Object.keys(response.data).length === 0) {
+                throw new Error('Unable to find gif')
+            }
+            img.src = response.data.images.original.url;
+        })
+        .catch(function (error) {
+            alert('Failure check console');
+            console.log(error);
+        });
+}
+getGif('cats');
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const input = document.querySelector('input#search');
+    let inputValue = input.value;
+    if (input.value === '') {
+        inputValue = 'cats'
+    } else {
+        inputValue = input.value
+    }
+    input.value = '';
+
+    getGif(inputValue);
+})
