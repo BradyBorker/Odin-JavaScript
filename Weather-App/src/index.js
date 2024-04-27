8bac6ff66aa9fe3afc542747954eba8663aa5564
Brady Barker <brady.d.barker@gmail.com>
2024-04-27 15:48:17 -0400

    Can enter a city. Returns a table of the forcasted weather

--- Weather-App/src/index.js
+++ Weather-App/src/index.js
@@ -1,5 +1,16 @@
-import { apiKey } from "./api-key";
+// eslint-disable-next-line import/extensions
+import getForcast from './fetchAsyncAwait';
+import displayWeatherReport from './displayWeatherReport';
+import './style.css';
 
 // Weather api example: https://api.weatherapi.com/v1/current.json?key=${key}&q=london
 
-import './fetchVanilla';
+const form = document.querySelector('form');
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const city = document.querySelector('input').value;
+    const forcastData = await getForcast(city);
+    console.log(forcastData);
+    displayWeatherReport(forcastData)
+})
